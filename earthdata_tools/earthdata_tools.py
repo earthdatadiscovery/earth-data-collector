f4a8a9838dd9da046a4a19df9e84bdd0dd278086
Samuel Jimenez <sjimenezre@gmail.com>
2020-10-04 01:02:18 +0200

    Refacto

---
+++ earthdata_tools/earthdata_tools.py
@@ -0,0 +1,44 @@
+import json
+import requests
+
+# Earth Data Config
+earthdata_endpoint = "https://cmr.earthdata.nasa.gov"
+img_url_prefix = "https://cmr.earthdata.nasa.gov/browse-scaler/browse_images/datasets/"
+
+def get_collection_ids(has_granules=None):
+
+    batch_size = 2000
+    params = {
+        "page_size": batch_size,
+        "offset": 0,
+    }
+    collection_ids = []
+    
+    print("Gathering collections IDs from CMR Earth Data API")
+    earthdata_url = earthdata_endpoint + "/search/collections/"
+    if has_granules is not None:
+        earthdata_url += "?has_granules=" + str(has_granules).lower()
+    response = requests.get(
+        earthdata_url,
+        headers={"Accept": "application/json"},
+        params=params,
+    )
+    if (response.status_code == 200):
+        results = json.loads(response.text)['feed']['entry']
+    else:
+        raise Exception("Couldn't access CMR Earth Data API")
+    while (len(results) > 0):
+
+        for dataset in results:
+            collection_ids.append(dataset['id'])
+
+        params['offset'] += batch_size
+        response = requests.get(
+            earthdata_endpoint + "/search/collections/",
+            headers={"Accept": "application/json"},
+            params=params,
+        )
+        if (response.status_code == 200):
+            results = json.loads(response.text)['feed']['entry']
+    print("Collections IDs retrieved")
+    return collection_ids
---
+++ meilisearch_tools/meilisearch_tools.py
@@ -0,0 +1,59 @@
+import requests
+import json
+import meilisearch
+
+from earthdata_tools.earthdata_tools import img_url_prefix, earthdata_endpoint
+
+# MeiliSearch Config
+meilisearch_client  = meilisearch.Client("http://localhost:7700", "")
+index = meilisearch_client.get_or_create_index("nasa", options={"primaryKey": "id"})
+indexing_batch_size = 50
+
+def build_document_data(result, collection_id, has_granules=None):
+    document = {}
+    document['id'] = collection_id
+    document['img_url'] = img_url_prefix + collection_id
+    document['title'] = result['title']
+    document['summary'] = result['summary']
+    document['short_name'] = result['short_name']
+    document['original_format'] = result['original_format']
+    document['organizations'] = result['organizations']
+    document['data_center'] = result['data_center']
+    if "links" in result:
+        document['links'] = result['links']
+    if "time_start" in result:
+        document['time_start'] = result['time_start']
+    if "time_end" in result:
+        document['time_end'] = result['time_end']
+    if has_granules is not None:
+        if has_granules:
+            document['granules'] = 1
+        else:
+            document['granules'] = 0
+    return document
+
+
+def index_collection(collection_ids, has_granules=None):
+    results = []
+
+    for collection_id in collection_ids:
+        try:
+            response = requests.get(
+                earthdata_endpoint + "/search/concepts/" + str(collection_id),
+                headers={"Accept": "application/json"},
+            )
+            if (response.status_code == 200):
+                result = json.loads(response.text)
+                document = build_document_data(result, collection_id, has_granules)
+                
+                results.append(document)
+            else:
+                raise Exception("Couldn't access CMR Earth Data API")
+            print(collection_id, "success", len(results))
+            
+        except Exception as e:
+            print("ERROR:", e)
+        if (len(results) >= indexing_batch_size):
+            index.add_documents(results)
+            results = []
+    index.add_documents(results)
