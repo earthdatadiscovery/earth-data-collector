bb1968bad8e5285ff8d21e9462252f094fba1985
Samuel Jimenez <sjimenezre@gmail.com>
2020-10-04 19:36:27 +0200

    Make categories and location failable

--- services/meilisearch/meilisearch.py
+++ services/meilisearch/meilisearch.py
@@ -6,12 +6,12 @@ from bs4 import BeautifulSoup
 from services.earthdata.earthdata import img_url_prefix, earthdata_endpoint
 
 # MeiliSearch Config
-meilisearch_client = meilisearch.Client("http://localhost:7700", "")
+meilisearch_client = meilisearch.Client("https://localhost:7700", "")
 index = meilisearch_client.get_or_create_index(
     "earthdata",
     options={"primaryKey": "id"}
 )
-indexing_batch_size = 50
+indexing_batch_size = 100
 
 
 def build_document(result, collection_id, has_granules=None):
@@ -20,15 +20,19 @@ def build_document(result, collection_id, has_granules=None):
 
     # Count granules if present
     granules = 0
-    if has_granules:
-        response = requests.get(
-            earthdata_endpoint + "/search/granules?collection_concept_id=" + collection_id,
-        )
-        if (response.status_code == 200):
-            from xml.dom import minidom
-            xmldoc = minidom.parseString(response.text)
-            items = xmldoc.getElementsByTagName('hits')[0]
-            granules = int(items.childNodes[0].data)
+    try:
+        if has_granules:
+            response = requests.get(
+                earthdata_endpoint + "/search/granules?collection_concept_id=" + collection_id,
+            )
+            if (response.status_code == 200):
+                from xml.dom import minidom
+                xmldoc = minidom.parseString(response.text)
+                items = xmldoc.getElementsByTagName('hits')[0]
+                granules = int(items.childNodes[0].data)
+    except Exception:
+        print("Couldn't get granules")
+
 
     # Scrapping Categories and Geopraphic location
     categories = []
@@ -41,29 +45,37 @@ def build_document(result, collection_id, has_granules=None):
         headers={'Accept':'text/html'}
     )
     if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        keywords_div = soup.find_all('div', class_='science-keywords-preview')
-        for div in keywords_div:
-            lists = div.find_all('ul', class_='arrow-tag-group-list')
-            for list in lists:
-                elem = list.find_all('li', class_='arrow-tag-group-item')
-                if not elem[1].string in categories:
-                    categories.append(elem[1].string)
-                if not elem[1].string in subcategories:
-                    subcategories[elem[1].string] = []
-                subcategories[elem[1].string].append(elem[2].string)
+        try:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            keywords_div = soup.find_all('div', class_='science-keywords-preview')
+            for div in keywords_div:
+                lists = div.find_all('ul', class_='arrow-tag-group-list')
+                for list in lists:
+                    elem = list.find_all('li', class_='arrow-tag-group-item')
+                    if not elem[1].string in categories:
+                        categories.append(elem[1].string)
+                    if not elem[1].string in subcategories:
+                        subcategories[elem[1].string] = []
+                    if (len(elem) >= 3):
+                        subcategories[elem[1].string].append(elem[2].string)
+        except Exception:
+            print("Couldn't scrape categories")
 
-        location_div = soup.find_all('ul', class_='location-keywords-preview')
-        for div in location_div:
-            lists = div.find_all('ul', class_='arrow-tag-group-list')
-            for list in lists:
-                elements = list.find_all('li', class_='arrow-tag-group-item')
-                for i in range(len(elements)):
-                    if elements[i].string == 'CONTINENT':
-                        if not elements[i+1].string in continents:
-                            continents.append(elements[i+1].string)
-                        if not elements[-1].string in countries:
-                            countries.append(elements[-1].string)
+        try:
+            location_div = soup.find_all('ul', class_='location-keywords-preview')
+            for div in location_div:
+                lists = div.find_all('ul', class_='arrow-tag-group-list')
+                for list in lists:
+                    elements = list.find_all('li', class_='arrow-tag-group-item')
+                    for i in range(len(elements)):
+                        if elements[i].string == 'CONTINENT':
+                            if not elements[i+1].string in continents:
+                                continents.append(elements[i+1].string)
+                            if not elements[-1].string in countries:
+                                countries.append(elements[-1].string)
+                            break
+        except Exception:
+            print("Couldn't scrape location data")
 
 
     document= {}
